0e5fa2828ddb9191da8a14a5259cdf4147764da1
EmCeeEs <ab22uhrnachtunruhe@gmail.com>
2022-01-26 22:49:25 +0100

    Add isRowComplete method.

--- Assets/Tests/Game.cs
+++ Assets/Tests/Game.cs
@@ -1,33 +1,33 @@
-// using System.Linq;
+using System.Linq;
 
 namespace Game {
     public class State {
 
-        private bool[,] _blocks;
         private int _rotationState = 0;
+        private bool[,] _blocks;
 
-        public State(int width, int height){
-            _blocks = new bool[width, height];
+        public State(int nRows, int nBlocksPerRow){
+            _blocks = new bool[nRows, nBlocksPerRow];
         }
 
-        private int width()
+        private int nRows()
         {
             return _blocks.GetLength(0);
         }
 
-        private int height()
+        private int nBlocksPerRow()
         {
             return _blocks.GetLength(1);
         }
 
         public void rotateLeft()
         {
-            _rotationState = mod(_rotationState+1, width());
+            _rotationState = mod(_rotationState+1, nBlocksPerRow());
         }
         
         public void rotateRight()
         {
-            _rotationState = mod(_rotationState-1, width());
+            _rotationState = mod(_rotationState-1, nBlocksPerRow());
         }
 
         public int getRotationState() {
@@ -49,21 +49,18 @@ namespace Game {
             return _blocks[row, col];
         }
 
-        // public bool[] getRow(int rowNumber)
-        // {
-        //     return _blocks[rowNumber,];
-        // }
+        public bool isRowComplete(int rowNumber)
+        {
+            return Enumerable.Range(0, nBlocksPerRow())
+                .Select(x => _blocks[rowNumber, x])
+                .All(x => x);
+        }
 
-        // public bool isRowComplete(int rowNumber) {
-        //     return getRow(rowNumber).All(x => x)
-        // }
-
-        // public void blowUpRow(int rowNumber) {
-        //     _blocks.Skip(rowNumber-1)
-        // }
+        public void blowUpRow(int rowNumber) {
+        }
 
         // helper function to implement modulo operation
-        // https://stackoverflow.com/questions/1082917/mod-of-negative-number-is-melting-my-brain
+        // https://stackoverflow.com/questions/1082917
         private static int mod(int k, int n)
         {
             return ((k %= n) < 0) ? k+n : k;
@@ -71,3 +68,19 @@ namespace Game {
     }
 }
 
+// https://stackoverflow.com/questions/27427527
+public class CustomArray<T> {
+    public T[] GetColumn(T[,] matrix, int columnNumber)
+    {
+        return Enumerable.Range(0, matrix.GetLength(0))
+                .Select(x => matrix[x, columnNumber])
+                .ToArray();
+    }
+
+    public T[] GetRow(T[,] matrix, int rowNumber)
+    {
+        return Enumerable.Range(0, matrix.GetLength(1))
+                .Select(x => matrix[rowNumber, x])
+                .ToArray();
+    }
+}
--- Assets/Tests/GameStateTest.cs
+++ Assets/Tests/GameStateTest.cs
@@ -11,17 +11,17 @@ using Game;
 
 public class GameStateTest
 {
-    const int WIDTH = 12;
-    const int HEIGHT = 6;
+    const int N_BLOCKS_PER_ROW = 12;
+    const int N_ROWS = 5;
 
     [Test]
     public void RotationLeftTest()
     {
-        Game.State state = new Game.State(WIDTH, HEIGHT);
+        Game.State state = new Game.State(N_ROWS, N_BLOCKS_PER_ROW);
 
         Assert.AreEqual(state.getRotationState(), 0);
 
-        RepeatAction(WIDTH, state.rotateLeft);
+        RepeatAction(N_BLOCKS_PER_ROW, state.rotateLeft);
         Assert.AreEqual(state.getRotationState(), 0,
             "Rotating left 12 times returns to starting postion.");
         
@@ -35,26 +35,26 @@ public class GameStateTest
     [Test]
     public void RotationRightTest()
     {
-        Game.State state = new Game.State(WIDTH, HEIGHT);
+        Game.State state = new Game.State(N_ROWS, N_BLOCKS_PER_ROW);
 
         Assert.AreEqual(state.getRotationState(), 0);
 
-        RepeatAction(WIDTH, state.rotateRight);
+        RepeatAction(N_BLOCKS_PER_ROW, state.rotateRight);
         Assert.AreEqual(state.getRotationState(), 0,
             "Rotating right 12 times returns to starting postion.");
 
         state.rotateRight();
-        Assert.AreEqual(state.getRotationState(), WIDTH-1);
+        Assert.AreEqual(state.getRotationState(), N_BLOCKS_PER_ROW-1);
 
         state.rotateRight();
-        Assert.AreEqual(state.getRotationState(), WIDTH-2);
+        Assert.AreEqual(state.getRotationState(), N_BLOCKS_PER_ROW-2);
 
     }
 
     [Test]
     public void ActivateBlockTest()
     {
-        Game.State state = new Game.State(WIDTH, HEIGHT);
+        Game.State state = new Game.State(N_ROWS, N_BLOCKS_PER_ROW);
 
         state.activateBlock(0, 0);
         Assert.IsTrue(state.isBlockActive(0, 0));
@@ -64,6 +64,31 @@ public class GameStateTest
 
         state.deactivateBlock(1, 1);
         Assert.IsFalse(state.isBlockActive(1, 1));
+    }
+
+    [Test]
+    public void IsRowCompleteTest()
+    {
+        Game.State state = new Game.State(10, 2);
+
+        Assert.IsFalse(state.isRowComplete(0), "Row is initially empty.");
+        
+        state.activateBlock(0, 0);
+        Assert.IsFalse(state.isRowComplete(0), "Row is still incomplete.");
+
+        state.activateBlock(0, 1);
+        Assert.IsTrue(state.isRowComplete(0), "Row is finally complete.");
+
+        state.deactivateBlock(0, 1);
+        Assert.IsFalse(state.isRowComplete(0), "Row is incomplete again.");
+    }
+
+    [Test]
+    public void BlowUpRowTest()
+    {
+        Game.State state = new Game.State(10, 2);
+
+        Assert.IsFalse(true);
     }
 
     private static void RepeatAction(int repeatCount, Action action)
