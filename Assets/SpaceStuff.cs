c4f9c999c658026dd8212ac7739dcc913cb8bc63
basti <donrigo2@gmail.com>
2022-02-14 20:50:34 +0100

    Added random stuffObject might go some trouble with git pull rebase...

---
+++ Assets/SpaceStuff.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceStuff : MonoBehaviour
+{
+    public GameObject[] randomStuff;
+    public Transform[] randomStuffSpawnLocations;
+    public Transform[] randomStuffRoationGO;
+
+    float delta;
+
+    void Start()
+    {
+        SpawnRandomStuff();
+    }
+
+    void FixedUpdate()
+    {
+        delta = Time.deltaTime;
+        RotateRandomStuff(delta);
+    }
+
+    public void SpawnRandomStuff()
+    {
+        for (int i = 0; i < randomStuffSpawnLocations.Length; i++)
+        {
+            GameObject planet = Instantiate(randomStuff[Random.Range(0, randomStuff.Length)], randomStuffSpawnLocations[i]);
+            randomStuffRoationGO[i].transform.Rotate(Vector3.up, Random.Range(0, 360));
+        }
+    }
+
+    public void RotateRandomStuff(float delta)
+    {
+        int direction = 1;
+        for (int i = 0; i < randomStuffRoationGO.Length; i++)
+        {
+            randomStuffRoationGO[i].transform.Rotate(Vector3.up, delta * direction);
+            direction *= -1;
+        }
+    }
+}
