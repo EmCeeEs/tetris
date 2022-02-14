d5ed58dc523420ec8c0d341e05ba7d7759517af0
EmCeeEs <ab22uhrnachtunruhe@gmail.com>
2022-02-14 03:16:05 +0100

    Add Actions, Reducer, Store & GameManager

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,100 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-using System.Linq;
 public class Player : MonoBehaviour
 {
-    private PlayerControls inputActions;
-    public Joystick joystick;
-
-    private Board board;
-
-    private int cooldownTimer = 0;
-    private const int MAX_COOLDOWN = 5;
+    private GameManager GM;
 
     private void Awake()
     {
-        board = GameObject.FindWithTag("Board").GetComponent<Board>();
+        GM = GameManager.Instance;
     }
 
-    private void OnEnable()
+    public void RotateBoard()
     {
-        if (inputActions == null)
-        {
-            inputActions = new PlayerControls();
-        }
-        inputActions.Enable();
+        State state = GM.Store.GetState();
+
+        // TODO: check validity
+
+        var action = ActionCreators.CreateRotateAction(1);
+        GM.Store.Dispatch(action);
     }
 
-    private void OnDisable()
+    public void InvertBlock()
     {
-        inputActions.Disable();
-    }
+        State state = GM.Store.GetState();
 
-    private void FixedUpdate()
-    {
-        if (cooldownTimer == 0)
-        {
-            HandleRotation();
-            HandleXInversion();
-        }
-        else
-        {
-            cooldownTimer -= 1;
-        }
-    }
+        // TODO: check validity
 
-    private void HandleXInversion()
-    {
-        bool invertion = inputActions.PlayerMovement.PlayerInvertBlockX.phase == InputActionPhase.Performed;
-
-        if (joystick.Vertical > 0.5 || joystick.Vertical < -0.5)
-        {
-            invertion = true;
-        }
-
-        if (board.currentBlock)
-        {
-            BlockParent block = board.currentBlock.GetComponent<BlockParent>();
-            List<Slot> newLayout = LayoutCreator.InvertX(block.BlockLayout);
-
-            bool canInvert = newLayout.All(slot => board.IsEmpty(slot + block.LowerSlot));
-
-            if (canInvert && invertion)
-            {
-                block.InvertX();
-
-                cooldownTimer = MAX_COOLDOWN;
-            }
-        }
-    }
-
-    private void HandleRotation()
-    {
-        bool moveLeft = inputActions.PlayerMovement.PlayerRotationLeft.phase == InputActionPhase.Performed;
-        bool moveRight = inputActions.PlayerMovement.PlayerRotationRight.phase == InputActionPhase.Performed;
-
-        if (joystick.Horizontal > 0.5)
-        {
-            moveLeft = true;
-        }
-        if (joystick.Horizontal < -0.5)
-        {
-            moveRight = true;
-        }
-
-        if (moveRight)
-        {
-            board.RotateRight();
-
-            cooldownTimer = MAX_COOLDOWN;
-        }
-        if (moveLeft)
-        {
-            board.RotateLeft();
-            cooldownTimer = MAX_COOLDOWN;
-        }
+        var action = ActionCreators.CreateInvertAction();
+        GM.Store.Dispatch(action);
     }
 }
